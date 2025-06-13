f19e9beea228c3eb962fee3e83dde469ff370dfa
lRethi <azraethly@gmail.com>
2025-06-13 09:33:36 -0300

    Aula8_Debug

---
+++ Aula8/Exercicio3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Aula8
+{
+    public struct Hero
+    {
+        public string Nome, Poder;
+        public int Pts;
+    }
+    static void Main()
+    {
+        Hero[] herois = new Hero[3];
+    }
+}
